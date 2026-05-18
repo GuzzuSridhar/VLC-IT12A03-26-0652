870ed7061747ba8209c23799f4da40d0ede865ce
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 12:49:23 +0530

    add special numbers

---
+++ Java Samples/src/basics/SpecialNumbers.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        // Java Supports the following number systems
+        /*
+         * 1. Binary (base 2)
+         * 2. Octal (base 8)
+         * 3. Hexadecimal (base 16)
+         */
+
+        // binary
+        int binary = 0b01;
+        System.out.println(binary);
+
+        // octal
+        int oct = 004;
+        System.out.println(oct);
+
+        // hexadecimal
+        int h = 0x0067;
+        System.out.println(h);
+
+    }
+}
