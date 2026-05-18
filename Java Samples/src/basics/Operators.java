be2ee757713e0b462976578b1139720c59a99751
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 15:01:42 +0530

    add operators

---
+++ Java Samples/src/basics/Operators.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class Operators {
+    public static void main(String[] args) {
+        /*
+         * Arithmetic
+         * Assignment
+         * Comparision
+         * Logical
+         * Shift
+         * bitwise
+         */
+
+        // Arithmetic Operators
+        // + - * / % ++ --
+
+        int x = 10, y = 20;
+
+        System.out.println(x + y); // addition
+        System.out.println(x - y); // Subtraction
+        System.out.println(x * y); // Multiplication
+        System.out.println(x / y); // Division
+        System.out.println(x % y); // Modulus
+    }
+}
