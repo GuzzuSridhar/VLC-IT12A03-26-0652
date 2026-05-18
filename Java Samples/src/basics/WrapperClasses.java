778a202dd9d682705d16309be9b37b92d22481be
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 13:18:44 +0530

    add wrapper classes

---
+++ Java Samples/src/basics/WrapperClasses.java
@@ -0,0 +1,40 @@
+package basics;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+
+        // every primitive type has a appropriate Object / class
+
+        // byte --> Byte
+        Byte b = 100;
+        System.out.println(b);
+
+        // short --> Short
+        Short s = 998;
+        System.out.println(s);
+
+        // int --> Integer
+        Integer i = 9899;
+        System.out.println(i);
+
+        // long --> Long
+        Long l = 10000l;
+        System.out.println(l);
+
+        // float -- > Float
+        Float f = 887.88f;
+        System.out.println(f);
+
+        // Double --> Double
+        Double d = 9899.777;
+        System.out.println(d);
+
+        // char --> Character
+        Character c = 'a';
+        System.out.println(c);
+
+        // boolean --> boolean
+        Boolean bol = true;
+        System.out.println(bol);
+    }
+}
