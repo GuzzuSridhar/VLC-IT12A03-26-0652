e117988bd47ec8619210ea35095b5a65040e87a9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 13:48:14 +0530

    add Strings

---
+++ Java Samples/src/basics/TextualData.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class TextualData {
+    public static void main(String[] args) {
+        // String the class / object type used to represent text in java
+        // declare Strings
+        // Strings are objects (NOT PRIMITIVE)
+
+        String name; // just declare and not initialize
+        name = "John"; // assigning value to the variable
+
+        String firstName = "Abraham"; // declare and initialize
+
+        System.out.println(name);
+        System.out.println(firstName);
+
+    }
+
+}
