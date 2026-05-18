f9353fc58cd8d1140e98a90b833aaa1cf90be85c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 13:32:39 +0530

    add max and min values

--- Java Samples/src/basics/MaxAndMinValues.java
+++ Java Samples/src/basics/MaxAndMinValues.java
@@ -1,5 +1,36 @@
 package basics;
 
 public class MaxAndMinValues {
-
+    public static void main(String[] args) {
+        System.out.println("-------------Byte Type----------------------");
+        System.out.println("The size of byte type is " + Byte.SIZE + " bits");
+        System.out.println("The min value of the byte type is " + Byte.MIN_VALUE);
+        System.out.println("The max value of the byte type is " + Byte.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------Short Type----------------------");
+        System.out.println("The size of Short type is " + Short.SIZE + " bits");
+        System.out.println("The min value of the Short type is " + Short.MIN_VALUE);
+        System.out.println("The max value of the Short type is " + Short.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------Integer Type----------------------");
+        System.out.println("The size of Integer type is " + Integer.SIZE + " bits");
+        System.out.println("The min value of the Integer type is " + Integer.MIN_VALUE);
+        System.out.println("The max value of the Integer type is " + Integer.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------Long Type----------------------");
+        System.out.println("The size of Long type is " + Long.SIZE + " bits");
+        System.out.println("The min value of the Long type is " + Long.MIN_VALUE);
+        System.out.println("The max value of the Long type is " + Long.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------Float Type----------------------");
+        System.out.println("The size of Float type is " + Float.SIZE + " bits");
+        System.out.println("The min value of the Float type is " + Float.MIN_VALUE);
+        System.out.println("The max value of the Float type is " + Float.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+        System.out.println("-------------Double Type----------------------");
+        System.out.println("The size of Double type is " + Double.SIZE + " bits");
+        System.out.println("The min value of the Double type is " + Double.MIN_VALUE);
+        System.out.println("The max value of the Double type is " + Double.MAX_VALUE);
+        System.out.println("-------------------------------------------");
+    }
 }
