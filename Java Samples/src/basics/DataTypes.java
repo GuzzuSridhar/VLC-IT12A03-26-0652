68d5b77dc98169f029de471cf1581db643e5a1b0
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-18 12:31:54 +0530

    add more material

--- Java Samples/src/basics/DataTypes.java
+++ Java Samples/src/basics/DataTypes.java
@@ -2,5 +2,54 @@ package basics;
 
 public class DataTypes {
 
-    // Java is dynamic typed language
+    public static void main(String[] args) {
+        // Java is dynamic typed language
+        // Java has 2 types (datatypes)
+        /*
+         * 1. Primitive Types
+         * 2. Object Types
+         */
+
+        /*
+         * 8 Primitive types
+         * 1. byte
+         * 2. short
+         * 3. int
+         * 4. long
+         * 5. float
+         * 6. Double
+         * 7. character
+         * 8. boolean
+         */
+
+        // byte is 8 bit signed integer
+        byte age = 20;
+        System.out.println(age);
+
+        // short is 16 bit signed integer
+        short shrt = 100;
+        System.out.println(shrt);
+
+        // int is 32 bit signed integer
+        int sal = 1000;
+        System.out.println(sal);
+
+        // long is 64 bit signed integer
+        long salesAmt = 190909;
+        System.out.println(salesAmt);
+        // float is 32 bit signed floating point value
+        float com = 189.66f;
+        System.out.println(com);
+        // double is 64 bit signed floating point value
+        double income = 8789.66;
+        System.out.println(income);
+        // char 16 bit unicode character
+        char c = 'a';
+        char x = '\u0079';
+        System.out.println(x);
+        // boolean i bit (true or false)
+        boolean isGood = true;
+        System.out.println(isGood);
+
+    }
 }
