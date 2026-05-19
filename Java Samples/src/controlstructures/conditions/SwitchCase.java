efe2f8e0c5019521539443ea6c9e6a3cbfbef335
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 10:18:40 +0530

    add switch

---
+++ Java Samples/src/controlstructures/conditions/SwitchCase.java
@@ -0,0 +1,53 @@
+package controlstructures.conditions;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        int dow = sc.nextInt();
+        switch (dow) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("invalid Entry");
+        }
+
+        // also display if the dow is a weekend or a work day
+        switch (dow) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Workday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("invalid Entry");
+        }
+    }
+}
