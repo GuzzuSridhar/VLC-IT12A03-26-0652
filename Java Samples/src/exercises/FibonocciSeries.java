1fbee2251b4f412e66ead02fc9679f7873776210
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 13:31:00 +0530

    add more exercises

---
+++ Java Samples/src/exercises/FibonocciSeries.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class FibonocciSeries {
+    public static void main(String[] args) {
+        // print fibonocci series upto 100
+        // 1 1 2 3 5 8 13 ......
+    }
+}
