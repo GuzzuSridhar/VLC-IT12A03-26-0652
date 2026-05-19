014e6dcdf87c2234d8d10dcec65fef9896926dd7
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 13:14:18 +0530

    add more exercises

---
+++ Java Samples/src/exercises/ReverseOfString.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class ReverseOfString {
+    public static void main(String[] args) {
+        // Accept a String from the user
+        // Print the reverse of the String
+    }
+}
