242cc484c51c28346b17f73b9b3049441580bc12
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 12:55:14 +0530

    add for loop

--- Java Samples/src/controlstructures/loops/Loops.java
+++ Java Samples/src/controlstructures/loops/Loops.java
@@ -10,13 +10,24 @@ public class Loops {
             System.out.print(num + " ");
             num++; // loop variable itration
         }
+        System.out.println();
 
         // Do While Loop
         int num1 = 1; // loop variable
         do {
-            System.out.println(num1);
+            System.out.print(num1 + " ");
             num1++; // loop variable itration
         } while (num1 < 11); // exit condition
+
+        System.out.println();
+
+        // for loop
+        // all three aspects(loop variable, Exit Condition, Iteration ) happen on same
+        // line
+        // loop variable is local to the loop and cannot be accessed after the loop.
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
 
     }
 }
