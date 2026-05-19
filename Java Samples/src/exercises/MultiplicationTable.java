e7c368c3746771d64800d6f016d55220927b658a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 13:28:26 +0530

    add more Exercises

---
+++ Java Samples/src/exercises/MultiplicationTable.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // accept a number from the user and print the multiplication table of that
+        // number
+    }
+}
---
+++ Java Samples/src/exercises/Palindrome.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        // Accept a String as a input
+        // find if the String is a Palindrome
+        // palindrome is a string which spells the same when read reverse also
+    }
+}
