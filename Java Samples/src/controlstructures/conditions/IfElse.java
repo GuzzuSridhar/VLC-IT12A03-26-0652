2e435807e12a88c4123e0f4d2a8375b79c9f2990
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 09:20:54 +0530

    add conditions

---
+++ Java Samples/src/controlstructures/conditions/IfElse.java
@@ -0,0 +1,25 @@
+package controlstructures.conditions;
+
+public class IfElse {
+    public static void main(String[] args) {
+        float salary = 9878.66f;
+
+        // based on the current salary, decide the increment
+
+        if (salary < 5000) {
+            salary += 1000;
+        } else if (salary > 5000 && salary < 7500) {
+            salary += 750;
+        } else if (salary > 7500 && salary < 10000) {
+            salary += 500;
+        } else {
+            salary += 250;
+        }
+
+        // if statement / block is mandatory
+        // we can have zero or more else if blocks
+        // we have zero or one else blocks
+
+        System.out.println(salary);
+    }
+}
