34fc044c11415c24d49a16e207b44b144ba5dfe7
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-19 12:18:19 +0530

    add exercises

--- Java Samples/src/exercises/MaxOfTwoNumbers.java
+++ Java Samples/src/exercises/MaxOfTwoNumbers.java
@@ -1,5 +1,26 @@
 package exercises;
 
-public class MaxOfTwoNumbers {
+import java.util.Scanner;
 
+public class MaxOfTwoNumbers {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        x = scan.nextInt();
+        System.out.print("Enter aother Number: ");
+        y = scan.nextInt();
+
+        // using if-else
+        if (x > y) {
+            System.out.println(x + " is greater");
+        } else {
+            System.out.println(y + " is greater");
+        }
+
+        // using Ternary
+        System.out.println((x > y) ? x + " is greater" : y + " is greater");
+
+        scan.close();
+    }
 }
