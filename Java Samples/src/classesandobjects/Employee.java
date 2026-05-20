968baa7fb846d57113aef5fdead3133947f3514a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-20 11:53:40 +0530

    add employees

--- Java Samples/src/classesandobjects/Employee.java
+++ Java Samples/src/classesandobjects/Employee.java
@@ -1,7 +1,5 @@
 package classesandobjects;
 
-import java.util.Scanner;
-
 public class Employee {
     String empId;
     String empName;
@@ -13,38 +11,6 @@ public class Employee {
         this.empName = empName;
         this.dept = dept;
         this.salary = salary;
-    }
-
-    public static void main(String[] args) {
-        Employee[] emp = new Employee[2];
-        Scanner sc = new Scanner(System.in);
-        String id, name, dep;
-        float sal;
-
-        for (int i = 0; i < emp.length; i++) {
-            // sc.nextLine();
-
-            System.out.print("Eneter the Employee ID: ");
-            id = sc.nextLine();
-
-            System.out.print("Eneter the Employee Name: ");
-            name = sc.nextLine();
-
-            System.out.print("Eneter the Employee Department: ");
-            dep = sc.nextLine();
-
-            System.out.print("Eneter the Employee Salary: ");
-            sal = sc.nextFloat();
-            sc.nextLine();
-
-            emp[i] = new Employee(id, name, dep, sal);
-        }
-
-        sc.close();
-
-        for (Employee e : emp) {
-            System.out.println(e.empName + " - " + e.empId + " - " + e.dept + " - " + e.salary);
-        }
     }
 
 }
---
+++ Java Samples/src/classesandobjects/EmployeeList.java
@@ -0,0 +1,38 @@
+package classesandobjects;
+
+import java.util.Scanner;
+
+public class EmployeeList {
+    public static void main(String[] args) {
+        Employee[] emp = new Employee[2];
+        Scanner sc = new Scanner(System.in);
+        String id, name, dep;
+        float sal;
+
+        for (int i = 0; i < emp.length; i++) {
+            // sc.nextLine();
+
+            System.out.print("Eneter the Employee ID: ");
+            id = sc.nextLine();
+
+            System.out.print("Eneter the Employee Name: ");
+            name = sc.nextLine();
+
+            System.out.print("Eneter the Employee Department: ");
+            dep = sc.nextLine();
+
+            System.out.print("Eneter the Employee Salary: ");
+            sal = sc.nextFloat();
+            sc.nextLine();
+
+            emp[i] = new Employee(id, name, dep, sal);
+        }
+
+        sc.close();
+
+        for (Employee e : emp) {
+            System.out.println(e.empName + " - " + e.empId + " - " + e.dept + " - " + e.salary);
+        }
+    }
+
+}
