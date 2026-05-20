b313488fb21141effce7f7ffbc7c70a8a03c1b39
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-20 09:21:43 +0530

    add constructors

--- Java Samples/src/classesandobjects/Book.java
+++ Java Samples/src/classesandobjects/Book.java
@@ -10,4 +10,29 @@ public class Book {
     int publicationYear;
     String genre;
     int numberofPages;
+
+    // constructors
+    // Default Constructor
+    public Book() {
+    }
+    // custom constructor
+
+    // fully loaded constructor
+    public Book(String title, String author, int publicationYear, String genre, int numberofPages) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.genre = genre;
+        this.numberofPages = numberofPages;
+    }
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public Book(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+
 }
--- Java Samples/src/classesandobjects/Library.java
+++ Java Samples/src/classesandobjects/Library.java
@@ -5,7 +5,9 @@ public class Library {
         // create object instances
         Book b001 = new Book();
         Book b002 = new Book();
-        Book b003 = new Book();
+        Book b003 = new Book("Design in Figma", "lilly", 1988, "design", 600);
+        Book b004 = new Book("Design in balsamiq", "bill Gates", 1999, "wifreames", 700);
+        Book b005 = new Book("New Book");
 
         // add information of each book instance b001
         b001.title = "Programming in HTML";
@@ -42,6 +44,17 @@ public class Library {
                 b002.publicationYear +
                 " " +
                 b002.numberofPages
+
+        );
+        System.out.println(b003.title +
+                " " +
+                b003.author +
+                " " +
+                b003.genre +
+                " " +
+                b003.publicationYear +
+                " " +
+                b003.numberofPages
 
         );
 
