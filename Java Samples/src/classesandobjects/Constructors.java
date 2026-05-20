81a8324b21e47a2710ac1fc0caf1c3864b936cef
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-20 09:03:12 +0530

    Add constructors

---
+++ Java Samples/src/classesandobjects/Constructors.java
@@ -0,0 +1,7 @@
+package classesandobjects;
+
+public class Constructors {
+    // constructors are used to simplify the object creation
+    // every class by default will have a constructor
+    // We can write any number of constructors in a class
+}
