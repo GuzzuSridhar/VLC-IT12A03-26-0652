1a40733b4d85b73a71a97c81ef2476efaaa6d394
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-20 14:19:35 +0530

    add more str methods

--- Java Samples/src/StringObject/StringMethods.java
+++ Java Samples/src/StringObject/StringMethods.java
@@ -14,5 +14,22 @@ public class StringMethods {
         System.out.println(str1.charAt(0));
         System.out.println("Character at the index 10 of the String " + str1 + " is " + str1.charAt(10));
 
+        // isEmpty -- return boolean check if the length of the string is 0
+        System.out.println(str1.isEmpty()); // false
+
+        // isBlank -- return boolean check if the string contains a blank space
+        System.out.println(str1.isBlank());
+
+        // equals
+        System.out.println(str1.equals(str2)); // false
+        System.out.println(str1.equalsIgnoreCase(str2)); // true
+
+        // startsWith endsWith
+        System.out.println(str1.startsWith("Java"));
+        System.out.println(str1.endsWith("Java"));
+
+        // String.format
+        System.out.printf("Language: %s, Version: %d", "Java", 21);
+
     }
 }
