12121551c80f1d12727b5c6eaf24cdaf8db4e34b
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-21 15:33:41 +0530

    update records

--- Java Samples/src/encapsulation/EmployeeData.java
+++ Java Samples/src/encapsulation/EmployeeData.java
@@ -2,10 +2,16 @@ package encapsulation;
 
 public class EmployeeData {
     public static void main(String[] args) {
+        // Usage of constructor
         Employee e = new Employee("Alex", 100);
         Employee e1 = new Employee("John", 100);
         Employee e2 = new Employee("Sara", 100);
 
+        // Getter Methods
+        System.out.println(e.name());
+        System.out.println(e.id());
+
+        // usage of toString()
         System.out.println(e);
         System.out.println(e1);
         System.out.println(e2);
