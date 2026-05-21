0906b20d566dd986e8e67d94285bc8beb5469ea3
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-21 10:29:25 +0530

    add am

---
+++ Java Samples/src/accessmodifier/packageone/ClassOne.java
@@ -0,0 +1,22 @@
+package accessmodifier.packageone;
+
+public class ClassOne {
+    // feilds
+    private int privateFeild;
+    public int publicFeild;
+    int defaultFeild;
+    protected int protectedFeild;
+
+    private void privateMethod() {
+    }
+
+    public void publicMethod() {
+    }
+
+    void defaultMethod() {
+    }
+
+    protected void protectedMethod() {
+    }
+
+}
