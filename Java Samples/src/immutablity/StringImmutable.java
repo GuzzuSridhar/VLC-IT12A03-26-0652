f7fe85536eec4542bd25f01e3190f0bd3dec586f
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-21 09:42:20 +0530

    append immute

--- Java Samples/src/immutablity/StringImmutable.java
+++ Java Samples/src/immutablity/StringImmutable.java
@@ -2,10 +2,14 @@ package immutablity;
 
 public class StringImmutable {
     public static void main(String[] args) {
+
+        // String is immutable
         String name = "Bill";
         System.out.println("Original Memory Address of String :" + name.hashCode());
         name = "John";
         System.out.println(" Changed Memory Address of String :" + name.hashCode());
+
+        // String builder is mutable
 
         StringBuilder fName = new StringBuilder("Bill");
         System.out.println("Original Memory Address of String builder:" + fName.hashCode());
