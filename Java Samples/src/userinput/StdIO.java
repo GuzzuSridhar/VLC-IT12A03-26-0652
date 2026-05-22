780319da2446ef2ac1c800d08b92602ba164673e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-05-22 09:56:23 +0530

    add stdio

---
+++ Java Samples/src/userinput/StdIO.java
@@ -0,0 +1,10 @@
+package userinput;
+
+public class StdIO {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+    }
+}
